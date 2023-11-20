9f1c649e2638917d7c804aeb6af571ea79d8ac40
IndyS1ngh <inderpreetsingh@hotmail.co.uk>
2023-11-20 16:14:35 +0000

    add get endpoints behaviour

--- app.js
+++ app.js
@@ -1,17 +1,18 @@
 const express = require("express");
-const { getTopics } = require("./controllers/controllers");
+const { getTopics, getEndpoints } = require("./controllers/controllers");
 
 const app = express();
 app.use(express.json());
 
 app.get("/api/topics", getTopics);
 
+app.get("/api", getEndpoints);
+
 app.all("*", (req, res) => {
   res.status(404).send({ msg: "path not found" });
 });
 
 app.use((err, req, res, next) => {
-  console.log(err);
   res.status(500).send({ msg: "internal server error" });
 });
 
