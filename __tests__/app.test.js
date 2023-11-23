37a557e71ba8b12cc9484a3a3c4e062ee4f6a95e
IndyS1ngh <inderpreetsingh@hotmail.co.uk>
2023-11-23 15:43:51 +0000

    complete task 12-including-comment-count

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -74,6 +74,7 @@ describe("GET /api/articles/:article_id", () => {
           votes: 100,
           article_img_url:
             "https://images.pexels.com/photos/158651/news-newsletter-newspaper-information-158651.jpeg?w=700&h=700",
+          comment_count: 11,
         });
       });
   });
