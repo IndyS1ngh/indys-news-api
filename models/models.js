f93e81e6abe4a48ffba7d496c842ffe04612a70d
IndyS1ngh <inderpreetsingh@hotmail.co.uk>
2023-11-23 10:23:34 +0000

    complete task 8-patch-article

--- models/models.js
+++ models/models.js
@@ -59,3 +59,18 @@ exports.insertComment = (article_id, newComment) => {
       return rows[0];
     });
 };
+
+exports.updateArticle = (article_id, newVote) => {
+  const { inc_votes } = newVote;
+  return db
+    .query(
+      `UPDATE articles SET votes = votes + $1 WHERE article_id = $2 RETURNING *;`,
+      [inc_votes, article_id]
+    )
+    .then(({ rows }) => {
+      if (!rows.length) {
+        return Promise.reject({ status: 404, msg: "not found" });
+      }
+      return rows[0];
+    });
+};
