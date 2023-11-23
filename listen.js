7fe27eaefdaa7b459e0b6c8e9f6c124c67a0fbea
IndyS1ngh <inderpreetsingh@hotmail.co.uk>
2023-11-23 16:40:53 +0000

    set up neccessary files for hosting

--- listen.js
+++ listen.js
@@ -1,5 +1,6 @@
 const app = require('./app');
+const { PORT = 9090 } = process.env;
 
-app.listen(9090, () => {
-    console.log('Server is listening on port 9090...');
+app.listen(PORT, () => {
+    console.log(`Listening on ${PORT}...`);
 });
