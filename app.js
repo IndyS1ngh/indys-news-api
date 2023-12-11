85cb6e7462314df1a068945e922f11bd1d2b9a75
IndyS1ngh <inderpreetsingh@hotmail.co.uk>
2023-12-11 10:28:25 +0000

    allow cross origin resource sharing

--- app.js
+++ app.js
@@ -4,8 +4,10 @@ const {
   handleCustomErrors,
   handleServerErrors,
 } = require("./errors");
+const cors = require("cors");
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 const apiRouter = require("./routes/api-router");
 
